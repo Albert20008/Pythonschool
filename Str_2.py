bccfe0ce57ee8adef1f3fc5eeb14fae240e5288a
Albert20008 <autardaev58@mail.ru>
2019-01-09 11:52:23 +0500

    Attempt 101

--- Str_2.py
+++ Str_2.py
@@ -1,8 +1,7 @@
 s = input('Введите что-нибудь: ')
 i = 0
 j = - 1
-start = True
-while start:
+while i < len(s) // 2:
 	while s[i] == ' ':
 		i = i + 1
 	while s[j] == ' ':
@@ -10,9 +9,9 @@ while start:
 	if s[i] == s[j]:
 		i = i + 1
 		j = j - 1
-		start = i < len(s)
 	else:
-		start = False
-		print('Это не Палиндром')
-if i == len(s):
+		break
+if i >= len(s) // 2:
 	print('Это Палиндром')
+else:
+	print('Это не Палиндром')
