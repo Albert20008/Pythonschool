91e26cdd7db77ed6f6b5f9e9b907d5f835297fcb
Albert20008 <autardaev58@mail.ru>
2019-01-11 18:51:53 +0500

    try 103

--- Str_2.py
+++ Str_2.py
@@ -2,7 +2,8 @@ s = input('Введите что-нибудь: ')
 i = 0
 j = - 1
 Palingrom = True
-for g in range(-1, len(s), 2):
+while i < len(s) // 2 + 1:
+#for g in range(-1, len(s), 2):
 	while s[i] == ' ':
 		i = i + 1
 	while s[j] == ' ':
