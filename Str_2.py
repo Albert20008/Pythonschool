64ac58dbee0588ad2101906e93abbf1b5e24fee1
Albert20008 <autardaev58@mail.ru>
2019-01-13 18:59:58 +0500

    try 105

--- Str_2.py
+++ Str_2.py
@@ -1,20 +1,25 @@
 s = input('Введите что-нибудь: ')
+
 i = 0
 j = - 1
-Palingrom = True
-while i < len(s) // 2 + 1:
-#for g in range(-1, len(s), 2):
+l = len(s) - 1
+
+while i <= l // 2:
+
 	while s[i] == ' ':
 		i = i + 1
+
 	while s[j] == ' ':
 		j = j - 1
+
 	if s[i] == s[j]:
 		i = i + 1
 		j = j - 1
 	else:
-		Palingrom = False
+		#Palingrom = False
 		break
-if Palingrom:
+
+if i > l // 2:
 	print('Это Палиндром')
 else:
 	print('Это не Палиндром')
